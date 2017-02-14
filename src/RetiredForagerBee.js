59e9c05a084ddfaf8f41363a0f5f86ca6cb92341
Anthony Bianco <anthony.bianco3@gmail.com>
2017-02-14 13:48:12 -0800

    Implement Bee classes with es6

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,15 @@
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends ForagerBee {
+  constructor () {
+  	super ();
+  	this.age = 40;
+  	this.color = 'grey';
+  	this.canFly = false;
+  	this.job = 'gamble';
+  }
+  forage () {
+  	return 'I am too old, let me play cards instead';
+  }
+  gamble (treasure) {
+  	this.treasureChest.push(treasure);
+  }
 };
